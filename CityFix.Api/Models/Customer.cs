efc5c2ee97eccff8317e0ceb7d00ee3d7b04077d
akhras018 <akhras018@gmail.com>
2026-04-06 19:27:20 +0300

    BSPMT14-11

---
+++ CityFix.Api/Models/Customer.cs
@@ -0,0 +1,13 @@
+namespace CityFix.Api.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string PasswordHash { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
